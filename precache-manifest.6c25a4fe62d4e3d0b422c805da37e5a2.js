e5a00548c28b0c3037c546b823bbb6ab691bd4ca
rsercano <ozdemirsercan27@gmail.com>
2019-03-15 17:44:29 +0000

    Auto generated commit

---
+++ precache-manifest.6c25a4fe62d4e3d0b422c805da37e5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/status-components/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6da0f013c6b736e8fe3",
+    "url": "/status-components/static/js/runtime~main.d6da0f01.js"
+  },
+  {
+    "revision": "cc441c20260b22081aa6",
+    "url": "/status-components/static/js/main.cc441c20.chunk.js"
+  },
+  {
+    "revision": "9a03d35e8bafc42cc333",
+    "url": "/status-components/static/js/1.9a03d35e.chunk.js"
+  },
+  {
+    "revision": "cc441c20260b22081aa6",
+    "url": "/status-components/static/css/main.cacba9d4.chunk.css"
+  },
+  {
+    "revision": "236ba487d0cb1bccd4d258a505f03a83",
+    "url": "/status-components/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56780cd02f4b28364c60675ae88b5d98.js"
+  "/status-components/precache-manifest.6c25a4fe62d4e3d0b422c805da37e5a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/status-components/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
